069cc0b62fc4959cd93a9e21fa1b902fe30b375d
David Cooper <daecoope@ucsc.edu>
2020-06-11 14:48:52 -0700

    David's First Commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http'); // http is a module
+var testModule = require("./modules/testModule");
 
 http.createServer(function (req,res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    res.end('Hello World!' + testModule.dateTime());
 }).listen(8080);
